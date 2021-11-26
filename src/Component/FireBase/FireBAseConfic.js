2b0fe477425abebb561635a369fc6ff93e089737
REDOY <74199587+555REDOY555@users.noreply.github.com>
2021-11-26 22:01:32 +0600

    ga

--- src/Component/FireBase/FireBAseConfic.js
+++ src/Component/FireBase/FireBAseConfic.js
@@ -1,12 +1,10 @@
-
-
 const FireBAseConfic = {
-     apiKey: "AIzaSyAjP_Q5GaDcu8Nu3Ked5RZAwObIC-hYpas",
-     authDomain: "akr-hospital.firebaseapp.com",
-     projectId: "akr-hospital",
-     storageBucket: "akr-hospital.appspot.com",
-     messagingSenderId: "587755710825",
-     appId: "1:587755710825:web:ace8af10fd94a27bd5f5f8"
+     apiKey: process.env.REACT_APP_API_KEY,
+     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+     projectId: process.env.REACT_APP_PROJECT_ID,
+     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+     appId: process.env.REACT_APP_APP_ID,
 };
 
 export default FireBAseConfic;
